40d1aebe9f15e699756c96fa356fc4075539c78e
Quill-Agent <nevergreengoblin@protonmail.com>
2026-07-18 11:44:47 -0700

    chore: point installer at latest source zip

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # and unpacks it into this folder. Run once after cloning.
 set -e
 cd "$(dirname "$0")"
-curl -L -o /tmp/soldier-strike-full.zip "https://d2ol7oe51mr4n9.cloudfront.net/user_3GEeqy4X8bGhvossH45YMdlgtZe/9d3e8549-9878-4de7-a278-e7fa7c2e1669.zip"
+curl -L -o /tmp/soldier-strike-full.zip "https://d2ol7oe51mr4n9.cloudfront.net/user_3GEeqy4X8bGhvossH45YMdlgtZe/b0638f4c-f8a5-4db7-81d5-edd3d97de0f5.zip"
 unzip -o /tmp/soldier-strike-full.zip -d .
 rm /tmp/soldier-strike-full.zip
 echo "Full game unpacked: server.js, public/, tools/, design/"
